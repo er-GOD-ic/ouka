62d4362219fbd40b703a4c584db62e0d1196e9be
er-GOD-ic <kamepower@outlook.jp>
2025-09-03 23:29:56 +0900

    list_input_devices関数の実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use std::io;
+use evdev::Device;
+
+fn list_input_devices() -> io::Result<Vec<Device>> {
+    let vec_devices = fs::read_dir("/dev/input/")?
+        .filter_map(|entry| entry.ok())
+        .filter(|e| e.path().to_string_lossy().starts_with("/dev/input/event"))
+        .filter_map(|e| Device::open(&e.path()).ok())
+        .collect::<Vec<_>>();
+    Ok(vec_devices)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let devices = list_input_devices();
 }
