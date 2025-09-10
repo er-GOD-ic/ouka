645dbdd00b2b60b78bbef70acbdd378a9e04d721
er-GOD-ic <kamepower@outlook.jp>
2025-09-10 23:05:27 +0900

    luaからdeviceを読み込みできるようにする。

--- src/lua_api.rs
+++ src/lua_api.rs
@@ -1,13 +1,25 @@
-use mlua::{Lua, Result};
+use std::fs;
+use std::env;
 use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
+use dotenv::dotenv;
+use mlua::{Lua, Result};
+
+pub fn load_lua() -> Lua {
+    let lua: Lua = Lua::new();
+    dotenv().ok();
+    let config_path = env::var("OUKA_CONFIG").unwrap() + "config.lua";
+    let code = fs::read_to_string(config_path).expect("ファイル読込失敗");
+    lua.load(&code).exec();
+    lua
+}
 
 pub struct RemapTable {
     pub simple: HashMap<String, String>,
 }
 
 /// LuaインタープリタとAPIの初期化
-pub fn init_bind(lua: &Lua, remap_table: Arc<Mutex<RemapTable>>) -> Result<()> {
+pub fn init_fn_bind(lua: &Lua, remap_table: Arc<Mutex<RemapTable>>) -> Result<()> {
     let remap_table_clone = Arc::clone(&remap_table);
     let bind = lua.create_function(move |_, (from, to): (String, String)| {
         let mut table = remap_table_clone.lock().unwrap();
@@ -16,4 +28,11 @@ pub fn init_bind(lua: &Lua, remap_table: Arc<Mutex<RemapTable>>) -> Result<()> {
     })?;
     lua.globals().set("bind", bind)?;
     Ok(())
+}
+
+pub fn device_name(lua: &Lua, key: &str) -> String {
+    match lua.globals().get::<_, String>(key) {
+        Ok(name) => name,
+        Err(e) => panic!("Luaのグローバル変数 'Device' が存在しません: {}", e),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ mod lua_api;
 mod device;
 
 fn main() {
-    let devices = device::find_device_by_name("Keychron");
+    let lua = lua_api::load_lua();
+
+    // This is the name of global variable. This have to set in your lua file.
+    // You can change this to whatever you want to call it. For example; Mouse, Numpad or Keyboard.
+    let key = "Device";
+
+    let devices = device::find_device_by_name(&lua_api::device_name(&lua, key));
     println!("=== device list matching ===");
     device::print_device_list(devices.as_deref().unwrap_or(&[]));
 }
